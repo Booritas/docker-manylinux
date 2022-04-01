fae60fbf4bec92d6b15c45f543f0f995786a3979
Stanislav Melnikov <stanislav.melnikov@gmail.com>
2022-04-01 12:02:13 +0200

    docker image build

--- docker/conan_install_deps.sh
+++ docker/conan_install_deps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # install external dependencies required for the build
-conan install -pr /root/projects/conan/Linux/linux_release -if /tmp -g txt /root/projects/conanfile.txt
+conan install -pr /root/projects/conan/Linux/linux_release -if /tmp -g txt --build=libgeotiff /root/projects/conanfile.txt
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-conan install -pr /root/projects/conan/Linux/linux_debug -if /tmp -g txt /root/projects/conanfile.txt
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+#conan install -pr /root/projects/conan/Linux/linux_debug -if /tmp -g txt --build=libgeotiff /root/projects/conanfile.txt
+#rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
